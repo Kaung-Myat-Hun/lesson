bfe2e3334e46cb119604910c1b5c6c3b94424ead
Kaung-Myat-Hun <kaungmyathun7@gmail.com>
2025-09-11 20:44:45 +0630

    update: storage function and array join and split

--- script.js
+++ script.js
@@ -6,21 +6,6 @@ const form = document.getElementById("form");
 let todos = [];
 let todoText = "";
 
-input.addEventListener("change" , (e)=> {
-    todoText = e.target.value;
-});
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if(todoText === ""){
-        alert("Cannot add empty string");
-        return ;
-    }
-    todos.push(todoText);
-    input.value = "";
-    loopRenderFunction();
-})
-
 const deleteTodo = (li) => {
     const filtered = todos.filter((item) => {
         return item !== li.srcElement.innerText
@@ -34,7 +19,38 @@ const loopRenderFunction = () => {
     for(let i =0 ;i < todos.length ; i++){
         const liTag = document.createElement("li");
         liTag.onclick = deleteTodo;
-        liTag.innerText = todos[i];
+        liTag.innerText = (i+1) + " " + todos[i];
         list.appendChild(liTag);
     }
+}
+
+function init(){
+    const localStorageData = localStorage.getItem("test");
+    if(localStorageData){
+        todos = localStorageData.split(",");
+        loopRenderFunction();
+    }
+}
+init();
+
+input.addEventListener("change" , (e)=> {
+    todoText = e.target.value;
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if(todoText === ""){
+        alert("Cannot add empty string");
+        return ;
+    }
+    todos.push(todoText);
+    input.value = "";
+    const joined = todos.join();
+    localStorage.setItem( "test" , joined );
+    loopRenderFunction();
+})
+
+function storageClear(){
+    localStorage.clear();
+    sessionStorage.clear();
 }
