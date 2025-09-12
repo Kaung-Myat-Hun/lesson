3b18d37612c45121be5b567f15dbe831a4df017c
Kaung-Myat-Hun <kaungmyathun7@gmail.com>
2025-09-12 21:35:45 +0630

    update: async await and side effect

--- script.js
+++ script.js
@@ -1,56 +1,106 @@
-const input = document.getElementById("input");
-const btn = document.getElementById("add-btn");
-const list = document.getElementById("todo-list");
-const form = document.getElementById("form");
+// const input = document.getElementById("input");
+// const btn = document.getElementById("add-btn");
+// const list = document.getElementById("todo-list");
+// const form = document.getElementById("form");
 
-let todos = [];
-let todoText = "";
+// let todos = [];
+// let todoText = "";
 
-const deleteTodo = (li) => {
-    const filtered = todos.filter((item) => {
-        return item !== li.srcElement.innerText
-    })
-    todos = filtered
-    loopRenderFunction();
-}
+// const deleteTodo = (li) => {
+//     const filtered = todos.filter((item) => {
+//         return item !== li.srcElement.innerText
+//     })
+//     todos = filtered
+//     loopRenderFunction();
+// }
 
-const loopRenderFunction = () => {
-    list.innerHTML = "";
-    for(let i =0 ;i < todos.length ; i++){
-        const liTag = document.createElement("li");
-        liTag.onclick = deleteTodo;
-        liTag.innerText = (i+1) + " " + todos[i];
-        list.appendChild(liTag);
-    }
-}
+// const loopRenderFunction = () => {
+//     list.innerHTML = "";
+//     for(let i =0 ;i < todos.length ; i++){
+//         const liTag = document.createElement("li");
+//         liTag.onclick = deleteTodo;
+//         liTag.innerText = (i+1) + " " + todos[i];
+//         list.appendChild(liTag);
+//     }
+// }
 
-function init(){
-    const localStorageData = localStorage.getItem("test");
-    if(localStorageData){
-        todos = localStorageData.split(",");
-        loopRenderFunction();
-    }
-}
-init();
+// function init(){
+//     const localStorageData = localStorage.getItem("test");
+//     if(localStorageData){
+//         todos = localStorageData.split(",");
+//         loopRenderFunction();
+//     }
+// }
+// init();
 
-input.addEventListener("change" , (e)=> {
-    todoText = e.target.value;
-});
+// input.addEventListener("change" , (e)=> {
+//     todoText = e.target.value;
+// });
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if(todoText === ""){
-        alert("Cannot add empty string");
-        return ;
-    }
-    todos.push(todoText);
-    input.value = "";
-    const joined = todos.join();
-    localStorage.setItem( "test" , joined );
-    loopRenderFunction();
-})
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     if(todoText === ""){
+//         alert("Cannot add empty string");
+//         return ;
+//     }
+//     todos.push(todoText);
+//     input.value = "";
+//     const joined = todos.join();
+//     localStorage.setItem( "test" , joined );
+//     loopRenderFunction();
+// })
 
-function storageClear(){
-    localStorage.clear();
-    sessionStorage.clear();
-}
+// function storageClear(){
+//     localStorage.clear();
+//     sessionStorage.clear();
+// }
+
+// // const arr = [ 7 , 4 ,5 ,1 , 9 ];
+// // arr.reverse();
+// // console.log(arr);
+
+// // const arr1 = [2 ,11, 12 , 14 , 20,13 ]
+// // arr1.sort();
+// // console.log(arr1);
+
+// const arr2 = [
+//     {
+//         name: "Bate Thar", 
+//         age : 29
+//     },
+//     {
+//         name: "Kaung",
+//         age: 28
+//     }
+// ]
+
+// const returnArrayValue = arr2.map((item) => { 
+//     // each array index new array return 
+//     return item.name // filter ( true / false )
+// });
+
+// console.log(returnArrayValue);
+
+// let string = "Hello World";
+
+// const obj = {
+//     name : "Bate Thar",
+//     age: 29, 
+//     work: () => {
+//         console.log("coding.......")
+//     },
+// }
+
+// obj.work();
+
+let variable = 3;
+const obj1 = {
+    name: "hi"
+};
+
+console.log( variable < 3 ? "Less than 3" : "Greater than 3" )
+
+console.log(variable <= 3 && "Equal or less than 3")
+
+console.log(obj1?.name); // object <= null / undefined (  ) // skipped 
+console.log("after error");
