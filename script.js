29e05074303ebcf5387e1a4d002ad9ea3bfc5864
Kaung-Myat-Hun <kaungmyathun7@gmail.com>
2025-09-22 21:40:52 +0630

    update: addnew add to cart function

--- script.js
+++ script.js
@@ -50,6 +50,9 @@ function render(){
         imageTag.classList.add('card-image');
         cardTag.classList.add('card');
         spanTag.classList.add("add-to-cart-button");
+        spanTag.addEventListener("click", ()=> {
+            AddToCart(itemList[i])
+        })
         logoTag.src = itemList[i].logo;
         logoTag.alt = itemList[i].logo;
         logoTag.classList.add('add-to-cart-logo');
@@ -68,3 +71,66 @@ function render(){
     }
 }
 render();
+
+const cartList = [];
+
+function AddToCart (item) { 
+    console.log(item)
+    const addedItem = {
+        count: 1,
+        price : item.itemPrice,
+        name: item.itemName,
+        img: item.itemImage
+    }
+    cartList.push(addedItem);
+    console.log('cart list', cartList);
+    cartRender();
+}
+
+function cartRender() {
+    const total = document.getElementById('total');
+    total.innerHTML = "Total : 0";
+    const cart = document.getElementById("cart");
+    cart.innerHTML = "";
+    let totalPrice = 0;
+    for(let i = 0; i < cartList.length; i++){
+        totalPrice = totalPrice + (cartList[i].price * cartList[i].count)
+        const mainDiv = document.createElement("div");
+        const imgTag = document.createElement("img");
+        const itemNameTag = document.createElement('p');
+        const minusTag = document.createElement("span");
+        const cartCountDiv = document.createElement('div');
+        const cartControl = document.createElement("div");
+        const plusTag = document.createElement("span");
+        const numberTag = document.createElement("span");
+        const priceTag = document.createElement("p");
+        imgTag.src=cartList[i].img;
+        imgTag.alt=cartList[i].img;
+        imgTag.classList.add('cart-image');
+        priceTag.classList.add('cart-price');
+        priceTag.innerText = `Price : ${cartList[i].price * cartList[i].count}`;
+        itemNameTag.innerText = cartList[i].name;
+        itemNameTag.classList.add('cart-item-name');
+        minusTag.innerText = "-";
+        minusTag.classList.add('cart-btn');
+        plusTag.innerText = "+";
+        plusTag.classList.add('cart-btn');
+        numberTag.innerText = cartList[i].count;
+        numberTag.classList.add('cart-btn');
+        numberTag.classList.add('cart-count-number');
+        cartControl.classList.add('cart-control');
+        cartControl.appendChild(minusTag);
+        cartControl.appendChild(numberTag);
+        cartControl.appendChild(plusTag);
+        cartCountDiv.classList.add('cart-count');
+        cartCountDiv.appendChild(itemNameTag);
+        cartCountDiv.appendChild(cartControl);
+        mainDiv.classList.add('cart-item');
+        mainDiv.appendChild(imgTag);
+        mainDiv.appendChild(cartCountDiv);
+        mainDiv.appendChild(priceTag);
+        // final render
+        cart.appendChild(mainDiv);
+    } 
+    total.innerText = `Total : ${totalPrice}`
+}
